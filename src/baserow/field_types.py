585d420a6b3d2a644ced9b94b58bee9c54dd34b7
alex-berlin-tv <hostettler@alex-berlin.de>
2024-03-01 19:01:21 +0100

    formula table field type implemented

--- src/baserow/field_types.py
+++ src/baserow/field_types.py
@@ -13,6 +13,33 @@ class NumberType(enum.Enum):
   DECIMAL = enum.auto()
 
 
+class TimeFormat(enum.Enum):
+  HOUR_12 = 12
+  HOUR_24 = 24
+
+
+class FormulaType(enum.Enum):
+  invalid = 'invalid'
+  text = 'text'
+  char = 'char'
+  button = 'button'
+  link = 'link'
+  date_interval = 'date_interval'
+  duration = 'duration'
+  date = 'date'
+  boolean = 'boolean'
+  number = 'number'
+  single_select = 'single_select'
+  multiple_select = 'multiple_select'
+  single_file = 'single_file'
+
+
+class DateFormat(enum.Enum):
+  EU = 'EU'
+  US = 'US'
+  ISO = 'ISO'
+
+
 @dataclasses.dataclass
 class SelectOption:
   id: int
@@ -24,6 +51,23 @@ class SelectOption:
 @dataclasses.dataclass
 class EMailTableField(TableField):
   pass
+
+
+@Union.register(TableField, 'formula')
+@dataclasses.dataclass
+class FormulaTableField(TableField):
+  nullable: bool
+  formula: str
+  number_decimal_places: t.Optional[int]
+  date_force_timezone: t.Optional[str]
+  date_time_format: t.Optional[str]
+  duration_format: t.Optional[str]
+  array_formula_type: t.Optional[str]
+  date_include_time: t.Optional[bool]
+  date_show_tzinfo: t.Optional[bool]
+  error: t.Optional[str]
+  date_format: t.Optional[str]
+  formula_type: t.Optional[str]
 
 
 @Union.register(TableField, 'text')
@@ -45,6 +89,11 @@ class NumberTableField(TableField):
   number_type: NumberType
 
 
+@Union.register(TableField, 'phone_number')
+@dataclasses.dataclass
+class PhoneTableField(TableField):
+  pass
+
 @Union.register(TableField, 'single_select')
 @dataclasses.dataclass
 class SingleSelectTableField(TableField):
@@ -54,6 +103,12 @@ class SingleSelectTableField(TableField):
 @Union.register(TableField, 'url')
 @dataclasses.dataclass
 class UrlTableField(TableField):
+  pass
+
+
+@Union.register(TableField, 'uuid')
+@dataclasses.dataclass
+class UUIDTableField(TableField):
   pass
 
 
