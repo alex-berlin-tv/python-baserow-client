4b740fc1004cf0cd55cebbc6754337dc8d25a0dd
alex-berlin-tv <hostettler@alex-berlin.de>
2024-03-04 12:39:24 +0100

    tests updated

--- src/baserow/field_types_test.py
+++ src/baserow/field_types_test.py
@@ -14,7 +14,7 @@ def test__TableField__can_deserialize_into_union_subtypes() -> None:
     assert dump(text_field, TableField) == text_field_data
     assert load(text_field_data, TableField) == text_field
 
-    number_field = NumberTableField(42, 1, 'foo', 0, False, False, 2, False, NumberType.INTEGER)
+    number_field = NumberTableField(42, 1, 'foo', 0, False, False, 2, False)
     number_field_data = {"type": "number", "id": 42, "table_id": 1, "name": "foo", "order": 0, "primary": False, "number_decimal_places": 2, "number_negative": False, "number_type": "INTEGER"}
     assert dump(number_field, TableField) == number_field_data
     assert load(number_field_data, TableField) == number_field
